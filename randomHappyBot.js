7a1c55cf851228f86d1189d59eef07168b8dba16
Bruce Johnson <bruce@yogainternational.com>
2020-01-13 19:33:25 -0500

    Add files via upload

--- randomHappyBot.js
+++ randomHappyBot.js
@@ -1,50 +1,57 @@
 $(document).ready(function() {
-  var verbsArr = ['ran through', 'ate', 'smelled', 'jumped over', 'sang to'];
+  var verbsArr = ["ran through", "ate", "smelled", "jumped", "sang to"];
   var adjectivesArr = [
-    'blue',
-    'flavorful',
-    'spontaneous',
-    'wildly energetic',
-    'fluffy',
-    'mysterious',
-    'random',
-    'happy',
-    'ecstatic',
-    'chipper',
+    "blue",
+    "flavorful",
+    "spontaneous",
+    "wildly energetic",
+    "fluffy"
   ];
-  var articlesArr = ['a', 'the'];
-  var nounsArr = [
-    'rainbow',
-    'horse',
-    'cloud',
-    'car',
-    'streetlight',
-    'puppy',
-    'chocolate bar',
-    'ice cream',
-    'kitty',
+  const lyAdjectivesArr = [
+    "unexpectedly",
+    "amazingly",
+    "strangely",
+    "annoyingly",
+    "trepidatiously"
   ];
-  var name = $('#name').text();
+  var articlesArr = ["a", "the"];
+  var nounsArr = ["rainbow", "horse", "cloud", "car", "streetlight", "puppy"];
+  var name = $("#name").text();
 
-  var _randomVerb = function() {
+  const _randomVerb = function() {
     return verbsArr[Math.floor(Math.random() * verbsArr.length)];
   };
 
-  var _randomAdjective = function() {
+  const _randomAdjective = function() {
     return adjectivesArr[Math.floor(Math.random() * adjectivesArr.length)];
   };
-  var _randomArticle = function() {
+  const _randomArticle = function() {
     return articlesArr[Math.floor(Math.random() * articlesArr.length)];
   };
-  var _randomNoun = function() {
+  const _randomNoun = function() {
     return nounsArr[Math.floor(Math.random() * nounsArr.length)];
   };
 
-  $('#buttonToClick').on('click', function() {
-    $('#text').text(() => {
+  const _randomLyAdjective = () =>
+    lyAdjectivesArr[Math.floor(Math.random() * lyAdjectivesArr.length)];
+
+  const capitalizeWord = word => {
+    const wordwithoutFirstLetter = word.slice(1, word.length);
+    const firstLetterCapped = word[0].toUpperCase();
+    return `${firstLetterCapped}${wordwithoutFirstLetter}`;
+  };
+
+  $("#buttonToClick").on("click", function() {
+    $("#text").text(() => {
       return `${$(
-        '#name'
-      ).val()} ${_randomVerb()} ${_randomArticle()} ${_randomAdjective()} ${_randomNoun()}`;
+        "#name"
+      ).val()} ${_randomVerb()} ${_randomArticle()} ${_randomAdjective()} ${_randomNoun()} while ${_randomArticle()} ${_randomNoun()} ${_randomVerb()}. 
+      ${capitalizeWord(_randomLyAdjective())}, ${$(
+        "#name"
+      ).val()} also ${_randomVerb()} ${_randomArticle()} ${_randomAdjective()} ${_randomNoun()}. 
+      ${$(
+        "#name"
+      ).val()} also ${_randomVerb()} ${_randomArticle()} ${_randomNoun()}.`;
     });
   });
 });
